3a010928824e7149706fa254c36f41f68a0d2fa6
Louis King <jinglemansweep@gmail.com>
2022-10-14 21:56:44 +0100

    Improved monitor script

--- home/.bin/xrandr.sh
+++ home/.bin/xrandr.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+disp_lenovo="DP-1-2"
+disp_iiyama_left="DP-1-3"
+disp_iiyama_right="DP-1-1"
+
 # L: Lenovo 2160p
-xrandr --output DP-1-2 --auto 
+xrandr --output ${disp_lenovo} --auto 
 # M: Iiyama 1080p
-xrandr --output DP-1-3 --auto --right-of DP-1-2
+xrandr --output ${disp_iiyama_left} --auto --right-of ${disp_lenovo}
 # R: Iiyama 1080p
-xrandr --output DP-1-1 --auto --right-of DP-1-3
+xrandr --output ${disp_iiyama_right} --auto --right-of ${disp_iiyama_left}
 
