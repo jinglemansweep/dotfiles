6c1247204228b5a25cd33279a4d9593218509b7f
Louis King <jinglemansweep@gmail.com>
2025-07-15 23:25:11 +0100

    feat(shell): add environment variables configuration script

---
+++ shell/.local/personal/scripts/env.sh
@@ -0,0 +1,12 @@
+# Shell
+export EDITOR="vim"
+export VISUAL="$EDITOR"
+
+# Docker
+export COMPOSE_BAKE=true
+
+# Python
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+
+# Claude
+export CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR=1
